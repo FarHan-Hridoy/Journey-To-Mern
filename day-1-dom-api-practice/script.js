c54113d62cca0fa2b8fa7d8a1457f210459bc4a2
Farhan-Hridoy <farhan8hridoy@gmail.com>
2025-05-17 10:10:53 +0600

    Day1

---
+++ day-1-dom-api-practice/script.js
@@ -0,0 +1,27 @@
+function loadUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => displayUsers(data))
+      .catch(error => console.error('Error:', error));
+  }
+  
+  function displayUsers(users) {
+    const container = document.getElementById('users-container');
+    container.innerHTML = ''; // আগের ডাটা ক্লিয়ার করা
+  
+    users.forEach(user => {
+      const div = document.createElement('div');
+      div.style.border = '1px solid #ccc';
+      div.style.margin = '10px';
+      div.style.padding = '10px';
+  
+      div.innerHTML = `
+        <h3>Name: ${user.name}</h3>
+        <p><strong>Email:</strong> ${user.email}</p>
+        <p><strong>Phone:</strong> ${user.phone}</p>
+      `;
+  
+      container.appendChild(div);
+    });
+  }
+  
